e4c3a9d11152e9e4486b30338acd1568d7f8d238
rookielxy <ryelovespython@gmail.com>
2018-10-30 10:32:38 +0800

    sliding window completed without hash table

--- MinimumWindowSubstring/main.cpp
+++ MinimumWindowSubstring/main.cpp
@@ -14,30 +14,47 @@ int main() {
     return 0;
 }
 
+
 string minWindow(const string &s, const string &t) {
-    if(s.empty() or t.empty())
+    if (s.empty() or t.empty())
         return "";
-    bool contain = new bool[t.size()];
-    for(int i = 0; i < t.size(); ++i)
-        contain[i] = false;
 
-    int left = 0, end = 0;
-    bool find = false;
-    while(not find) {
-        for(int i = 0; i < t.size(); ++i) {
-            if(s[end] == t[i])
-                contain[i] = true;
-        }
-        ++end;
-
-        int i = 0;
-        for(; i < t.size(); ++i) {
-            if(not contain[i])
-                break;
-        }
-        if(i == t.size())
-            find = true;
+    int remains[256];
+    bool contains[256];
+    memset(remains, 0, sizeof(remains));
+    memset(contains, false, sizeof(contains));
+    for (const auto ch : t) {
+	    ++remains[ch];
+        contains[ch] = true;
     }
 
-    
+    bool everFind = false;
+    int counter = 0;
+    int start = 0, end = 0, left = 0, len = (int)s.size();
+    while (start < s.size() or end < s.size()) {
+    	if (counter < t.size()) {
+    		if (contains[s[end]]) {
+			    counter += (remains[s[end]] > 0)? 1 : 0;
+			    --remains[s[end]];
+    		}
+    		if (end == s.size())
+    			break;
+		    ++end;
+    	} else {
+    		everFind = true;
+    		if (end - start < len) {
+    			left = start;
+    			len = end - start;
+    		}
+    		if (contains[s[start]]) {
+    			counter -= (remains[s[start]] < 0)? 0 : 1;
+    			++remains[s[start]];
+    		}
+    		++start;
+    	}
+    }
+    if (not everFind)
+    	return "";
+    else
+    	return s.substr((unsigned)left, (unsigned)len);
 }
