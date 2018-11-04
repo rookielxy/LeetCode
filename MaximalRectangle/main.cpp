1994c06678d2417180f3502909138533ed788c10
rookielxy <ryelovespython@gmail.com>
2018-11-04 15:02:57 +0800

    2-D into 1-D and solved

--- MaximalRectangle/main.cpp
+++ MaximalRectangle/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 int maximalRectangle(vector<vector<char>>& matrix);
+int largestRectangleArea(vector<int> heights);
 
 int main() {
 	int row, col;
@@ -23,59 +25,46 @@ int main() {
 }
 
 int maximalRectangle(vector<vector<char>>& matrix) {
-	if (matrix.empty())
+	if (matrix.empty() or matrix[0].empty())
 		return 0;
-
-	struct Cord {
-		int x, y;
-		Cord(): x(-1), y(-1) {}
-	};
-
-	auto table = new Cord*[matrix.size()];
-	for (int i = 0; i < matrix.size(); ++i)
-		table[i] = new Cord[matrix[i].size()];
-
 	int area = 0;
-	if (matrix[0][0] == '1') {
-		table[0][0].x = table[0][0].y = 0;
-		area = 1;
-	}
-	for (int j = 1; j < matrix[0].size(); ++j) {
-		if(matrix[0][j] == '0')
-			continue;
+	vector<vector<int>> heights;
 
-		table[0][j].x = 0;
-		table[0][j].y = (matrix[0][j - 1] == '0')? j : table[0][j - 1].y;
-		area = max(area, j - table[0][j].y + 1);
-	}
+	vector<int> line(matrix[0].size());
+	for (int i = 0; i < line.size(); ++i)
+		line[i] = matrix[0][i] == '1'? 1 : 0;
+	heights.emplace_back(line);
 	for (int i = 1; i < matrix.size(); ++i) {
-		if (matrix[i][0] == '0')
-			continue;
-
-		table[i][0].x = (matrix[i - 1][0] == '0')? i : table[i - 1][0].y;
-		table[i][0].y = 0;
-		area = max(area, i - table[i][0].x + 1);
-	}
-
-
-	for (int i = 1; i < matrix.size(); ++i) {
-		for (int j = 1; j < matrix[i].size(); ++j) {
+		for (int j = 0; j < matrix[i].size(); ++j) {
 			if (matrix[i][j] == '0')
-				continue;
-			if (matrix[i][j - 1] == '1' and matrix[i - 1][j] == '1') {
-				// TODO: induction to do
-			} else if (matrix[i][j - 1] == '1') {
-				table[i][j].x = i;
-				table[i][j].y = table[i][j - 1].y;
-			} else if (matrix[i - 1][j] == '1') {
-				table[i][j].x = table[i - 1][j].x;
-				table[i][j].y = j;
-			} else {
-				table[i][j].x = i;
-				table[i][j].y = j;
-			}
-			area = max(area, (i - table[i][j].x + 1)*(j - table[i][j].y + 1));
+				line[j] = 0;
+			else
+				line[j] = heights[i - 1][j] + 1;
 		}
+		heights.emplace_back(line);
 	}
+	for (int i = 0; i < matrix.size(); ++i)
+		area = max(area, largestRectangleArea(heights[i]));
 	return area;
+}
+
+int largestRectangleArea(vector<int> heights) {
+	if (heights.empty())
+		return 0;
+	if (heights.size() == 1)
+		return heights[0];
+
+	heights.emplace_back(0);
+	stack<int> bound;
+	int ret = 0;
+	for (int i = 0; i < heights.size(); ++i) {
+		while (not bound.empty() and heights[i] < heights[bound.top()]) {
+			int lower = heights[bound.top()];
+			bound.pop();
+			int length = bound.empty()? i : i - bound.top() - 1;
+			ret = max(ret, length*lower);
+		}
+		bound.push(i);
+	}
+	return ret;
 }
