4949fb00b75c416279f7b6407fc3c826e5505bbe
rookielxy <ryelovespython@gmail.com>
2018-12-31 00:05:06 +0800

    remove duplicate processes

--- Multiply/main.cpp
+++ Multiply/main.cpp
@@ -17,48 +17,27 @@ int main() {
 	return 0;
 }
 
-string add(string num1, string num2) {
-	string sum;
-	if (num1.size() < num2.size())
-		num1.insert(num1.size(), num2.size() - num1.size(), '0');
-	if (num1.size() > num2.size())
-		num2.insert(num2.size(), num1.size() - num2.size(), '0');
-	int count = 0;
-	for (int i = 0; i < num1.size(); ++i) {
-		int n1 = charToDigit(num1[i]), n2 = charToDigit(num2[i]);
-		char ch = digitToChar((n1 + n2 + count)%10);
-		count = (n1 + n2 + count)/10;
-		sum.push_back(ch);
-	}
-	if (count != 0)
-		sum.push_back(digitToChar(count));
-	return sum;
-}
 
 string multiply(string num1, string num2) {
 	if (num1.size() < num2.size())
 		return multiply(num2, num1);
 
-	string ret;
-	string b;
+	string ret(num1.size() + num2.size(), '0');
 	reverse(num1.begin(), num1.end());
 	reverse(num2.begin(), num2.end());
-	for (char ch : num2) {
-		string temp;
-		string base;
-		int factor1 = charToDigit(ch);
-		for (auto ch2 : num1) {
-			int factor2 = charToDigit(ch2);
-			int result = factor1*factor2;
-			if (result/10 > 0)
-				temp = add(temp, base + digitToChar(result%10) + digitToChar(result/10));
-			else
-				temp = add(temp, base + digitToChar(result%10));
-			base += '0';
+	int carry = 0;
+	for (int i = 0; i < num2.size(); ++i) {
+		int factor = charToDigit(num2[i]);
+		for (int j = 0; j < num1.size(); ++j) {
+			int product = charToDigit(ret[i + j]) + factor*charToDigit(num1[j]) + carry;
+			ret[i + j] = digitToChar(product%10);
+			carry = product/10;
 		}
-		temp = b + temp;
-		ret = add(ret, temp);
-		b += '0';
+		if (carry != 0) {
+			int product = charToDigit(ret[i + num1.size()]) + carry;
+			ret[i + num1.size()] = digitToChar(product%10);
+			carry = product/10;
+		}
 	}
 	while (ret.back() == '0' and ret.size() > 1)
 		ret.pop_back();
@@ -66,12 +45,12 @@ string multiply(string num1, string num2) {
 	return ret;
 }
 
-int charToDigit(char ch) {
+inline int charToDigit(char ch) {
 	assert(ch >= '0' and ch <= '9');
 	return ch - '0';
 }
 
-char digitToChar(int i) {
+inline char digitToChar(int i) {
 	assert(i >= 0 and i <= 9);
 	return (char)(i + '0');
 }
