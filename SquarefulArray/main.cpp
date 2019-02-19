bc02da3b84c7a324816194f1b830a7a6fe04ae70
rookielxy <ryelovespython@gmail.com>
2019-02-19 16:01:42 +0800

    brute force DFS

--- SquarefulArray/main.cpp
+++ SquarefulArray/main.cpp
@@ -3,12 +3,14 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 int numSquarefulPerms(vector<int>& A);
-int dfs(int u);
+int dfs(int u, int pre, int depth);
 bool isSquare(int n);
+int factorial(int n);
 
 int main() {
 	stringstream ss;
@@ -30,43 +32,80 @@ int main() {
  * squareful permutations are equal to a path consist of all nodes.
  * */
 
-struct Node {
-	bool visited;
-	vector<int> edges;
-	Node() : visited(false) {}
-};
 
-vector<Node> nodes;
+vector<vector<int>> nodes;
+vector<bool> contains;
 int numSquarefulPerms(vector<int>& A) {
 	if (A.size() <= 1)
 		return 0;
+	bool same = true;
+	for (int &ele : A) {
+		same = same && (ele == A.front());
+	}
+	if (same)
+		return isSquare(2*A[0])? 1 : 0;
 	nodes.resize(A.size());
+	contains.resize(A.size());
+	for (bool ele : contains)
+		ele = false;
 	for (int i = 0; i < A.size(); ++i) {
 		for (int j = i + 1; j < A.size(); ++j) {
 			if (isSquare(A[i] + A[j])) {
-				nodes[i].edges.emplace_back(j);
-				nodes[j].edges.emplace_back(i);
+				nodes[i].emplace_back(j);
+				nodes[j].emplace_back(i);
 			}
 		}
 	}
+
+	int end = 0;
+	for (vector<int> &edge : nodes) {
+		if (edge.empty())
+			return 0;
+		if (edge.size() == 1)
+			++end;
+	}
+	if (end > 2)
+		return 0;
+
 	int ret = 0;
 	for (int i = 0; i < A.size(); ++i) {
-		for (Node &node : nodes)
-			node.visited = false;
-		ret += dfs(i);
+		ret += dfs(i, -1, 1);
 	}
+
+	// there may still be duplicate permutations
+	map<int, int> counter;
+	for (int ele : A) {
+		if (counter.find(ele) == counter.end())
+			counter.emplace(ele, 1);
+		else
+			++counter.find(ele)->second;
+	}
+	int times = 1;
+	for (pair<int, int> ele : counter)
+		times *= factorial(ele.second);
+	return ret/times;
+}
+
+int dfs(int u, int pre, int depth) {
+	if (depth == nodes.size())
+		return 1;
+	int ret = 0;
+	contains[u] = true;
+	for (int v : nodes[u]) {
+		if (v == pre)
+			continue;
+		if (not contains[v])
+			ret += dfs(v, u, depth + 1);
+	}
+	contains[u] = false;
 	return ret;
 }
 
-int dfs(int u) {
-	nodes[u].visited = true;
-	int ret = 0;
-	for (int v : nodes[u].edges) {
-		if (not nodes[v].visited) {
-			ret += dfs(v);
-		}
-	}
-	return ret;
+int factorial(int n) {
+	int ans = 1;
+	for (int i = 2; i <= n; ++i)
+		ans *= i;
+	return ans;
 }
 
 bool isSquare(int n) {
