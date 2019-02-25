4ec61a44b5a97e6583a751f85215b675e6b30c35
rookielxy <ryelovespython@gmail.com>
2019-02-25 12:48:37 +0800

    minor improvement

--- Gridillumination/main.cpp
+++ Gridillumination/main.cpp
@@ -31,23 +31,27 @@ int main() {
 	return 0;
 }
 
-map<int, set<int>> h;
-map<int, set<int>> v;
-map<int, set<int>> d1;
-map<int, set<int>> d2;
+
 vector<int> gridIllumination(int N, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
 	vector<int> answer(queries.size());
 
+	map<int, int> h;
+	map<int, int> v;
+	map<int, int> d1;
+	map<int, int> d2;
+	set<pair<int, int>> turnOff;
+
 	for (vector<int> &lamp : lamps) {
-		h[lamp[0]].insert(lamp[1]);
-		v[lamp[1]].insert(lamp[0]);
-		d1[lamp[1] - lamp[0] + N - 1].insert(lamp[0]);
-		d2[lamp[0] + lamp[1]].insert(lamp[0]);
+		++h[lamp[0]];
+		++v[lamp[1]];
+		++d1[lamp[0] - lamp[1]];
+		++d2[lamp[0] + lamp[1]];
+		turnOff.insert(make_pair(lamp[0], lamp[1]));
 	}
 
 	for (int i = 0; i < queries.size(); ++i) {
 		int x = queries[i][0], y = queries[i][1];
-		if (h[x].empty() and v[y].empty() and d1[y - x + N - 1].empty() and d2[x + y].empty()) {
+		if (h[x] == 0 and v[y] == 0 and d1[x - y] == 0 and d2[x + y] == 0) {
 			answer[i] = 0;
 			continue;
 		} else {
@@ -55,19 +59,17 @@ vector<int> gridIllumination(int N, vector<vector<int>>& lamps, vector<vector<in
 		}
 		for (int row = x - 1; row <= x + 1; ++row) {
 			for (int col = y - 1; col <= y + 1; ++col) {
-				erasePoint(row, col, N);
+				if (row >= 0 and row < N and col >= 0 and col < N) {
+					if (turnOff.count(make_pair(row, col))) {
+						--h[row];
+						--v[col];
+						--d1[row - col];
+						--d2[row + col];
+						turnOff.erase(make_pair(row, col));
+					}
+				}
 			}
 		}
 	}
 	return answer;
 }
-
-void erasePoint(int x, int y, int N) {
-	if (x >= 0 and y >= 0 and x < N and y < N) {
-		h[x].erase(y);
-		v[y].erase(x);
-		d1[y - x + N - 1].erase(x);
-		d2[x + y].erase(x);
-	}
-}
-
