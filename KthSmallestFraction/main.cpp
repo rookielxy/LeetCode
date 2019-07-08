bd30cc3fd801e486c810513470493e11e38c5935
rookielxy <ryelovespython@gmail.com>
2019-07-08 09:46:00 +0800

    using binary search to reduce complexity

--- KthSmallestFraction/main.cpp
+++ KthSmallestFraction/main.cpp
@@ -26,6 +26,11 @@ int main() {
 	return 0;
 }
 
+/**
+ * 使用优先级队列，迭代K次，
+ * 假定数组有序条件下且K >> A.length，最坏时间复杂度为O(K*log K)。
+ */
+/*
 vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
 	sort(A.begin(), A.end());
 	struct Compare {
@@ -53,4 +58,44 @@ vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
 			heap.emplace(make_pair(result.first + 1, result.second));
 	}
 	return vector<int>{A[result.first], A[result.second]};
+}
+ */
+
+/**
+ * 二分搜索，直到找到阈值middle使小于middle的分数刚好为K个，
+ * 最坏时间复杂度为O(A.length*log K)
+ */
+vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+	sort(A.begin(), A.end());
+	double start = 0.0, end = 1.0;
+
+	while (start < end) {
+		double middle = start + (end - start)/2;
+		int numeIdx = 0, counter = 0, i = 0;
+		int col = 0, row = (int)A.size() - 1;   // 记录counter个小于middle的数中最大值
+		for (i = 0; i < (int)A.size(); ++i) {
+			while (true) {
+				if (numeIdx == i or (double)A[numeIdx]/A[i] > middle) {
+					if (numeIdx > 0 and A[numeIdx - 1]*A[row] > A[col]*A[i]) {
+						col = numeIdx - 1;
+						row = i;
+					}
+					counter += numeIdx;
+					break;
+				}
+				++numeIdx;
+			}
+
+			if (counter > K)
+				break;
+		}
+		if (counter > K)
+			end = middle;
+		else if (counter == K)
+			return vector<int>{A[col], A[row]};
+		else
+			start = middle;
+	}
+	// meaningless return to make compiler happy
+	return vector<int>{0, 0};
 }
