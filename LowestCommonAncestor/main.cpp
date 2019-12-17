ce4f95bf8c79d4e608b3fdf1e6b42b1426a62b20
rookielxy <rookielxy@gmail.com>
2019-12-17 18:49:01 +0800

    test global configuration

---
+++ LowestCommonAncestor/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left, *right;
+	explicit TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+int main() {
+
+	return 0;
+}
+
+TreeNode* lcaDeepestLeaves(TreeNode* root) {
+
+}
