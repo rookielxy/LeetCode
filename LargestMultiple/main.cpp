13ce81604451fb8b21d118f28295ea3c19142b55
rookielxy <ryelovespython@gmail.com>
2020-07-14 23:36:38 +0800

    change to math solution, 48 ms

--- LargestMultiple/main.cpp
+++ LargestMultiple/main.cpp
@@ -8,11 +8,12 @@ using namespace std;
 string largestMultipleOfThree(vector<int>& digits);
 
 int main() {
-    vector<int> digits = {0, 0, 0, 0, 0};
+    vector<int> digits = {9, 8, 8, 6, 6};
     cout << largestMultipleOfThree(digits) << endl;
     return 0;
 }
 
+/*
 string largestMultipleOfThree(vector<int>& digits) {
     sort(digits.begin(), digits.end(), [](int ele1, int ele2) -> bool { return ele1 > ele2; });
 
@@ -81,4 +82,55 @@ string largestMultipleOfThree(vector<int>& digits) {
         answer.push_back('0' + three[i]);
 
     return answer;
+}
+*/
+
+string largestMultipleOfThree(vector<int>& digits) {
+    sort(digits.begin(), digits.end(), [](int ele1, int ele2) -> bool { return ele1 > ele2; });
+    if (digits[0] == 0)
+        return "0";
+
+    int zeros = 0;
+    vector<int> remainder(3, 0);
+    for (int ele : digits) {
+        remainder[ele % 3] += 1;
+        if (ele == 0)
+            ++zeros;
+    }
+
+    if ((remainder[1] == 0 and remainder[2] < 3) or
+         (remainder[1] < 3 and remainder[2] == 0)) {
+        if (remainder[0] == 0)
+            return "";
+        if (remainder[0] == zeros)
+            return "0";
+    }
+
+    int sum = remainder[1] + 2*remainder[2];
+    switch (sum % 3) {
+        case 0: break;
+        case 1:
+            if (remainder[1] > 0)
+                --remainder[1];
+            else
+                remainder[2] -= 2;
+            break;
+        case 2:
+            if (remainder[2] > 0)
+                --remainder[2];
+            else
+                remainder[1] -= 2;
+            break;
+        default: assert(false);
+    }
+
+    string ret(remainder[0] + remainder[1] + remainder[2], '0');
+    int idx = 0;
+    for (int ele : digits) {
+        if (remainder[ele % 3] > 0) {
+            ret[idx++] = (char)(ele + '0');
+            remainder[ele % 3] -= 1;
+        }
+    }
+    return ret;
 }
